26f5578693fd6de18bfca887268c2fce316ca522
Critopher Diaz <cristopherdiaz.c@hotmail.com>
2024-08-18 02:54:56 -0400

    add: template views

--- controllers/PageController.js
+++ controllers/PageController.js
@@ -1,5 +1,26 @@
 const mainView = (req, res) => {
-    res.render('layout')
+    res.render('dashboard')
+}
+const iconsView = (req, res) => {
+    res.render('icons')
+}
+const mapsView = (req, res) => {
+    res.render('maps')
+}
+const notificationsView = (req, res) => {
+    res.render('notifications')
+}
+const userView = (req, res) => {
+    res.render('user')
+}
+const tablesView = (req, res) => {
+    res.render('tables')
+}
+const typographyView = (req, res) => {
+    res.render('typography')
+}
+const rtlView = (req, res) => {
+    res.render('rtl')
 }
 
-module.exports = mainView
+module.exports = { mainView, iconsView, mapsView, notificationsView, userView, tablesView, typographyView, rtlView }
--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,25 @@
 const express = require('express')
-const mainView = require('../controllers/PageController')
+const {
+  mainView,
+  iconsView,
+  mapsView,
+  notificationsView,
+  userView,
+  tablesView,
+  typographyView,
+  rtlView
+} = require('../controllers/PageController')
 console.log(mainView)
 
 const router = express.Router()
 
 router.get('/', mainView)
+router.get('/icons', iconsView)
+router.get('/maps', mapsView)
+router.get('/notifications', notificationsView)
+router.get('/user', userView)
+router.get('/tables', tablesView)
+router.get('/typography', typographyView)
+router.get('/rtl', rtlView)
 
 module.exports = { routes: router }
